ec3fb2a71f5b6db3b78bd569de2a2785b9a2e5eb
Oscar Ramirez Gutierrez <rg.oscar17@ciencias.unam.mx>
2024-04-06 08:09:17 -0600

    feat: pantallas datos y pedido

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,24 +2,23 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bEnviar;
+
+    EditText etNombre, etTelefono, etCorreo, etCalle, etNumInt, etNumExt, etCP, etAlcaldia, etEstado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-/*        TextView dibujo = findViewById(R.id.dibujo);
-        String tux =
-                "        .--.\n" +
-                "       |o_o |\n" +
-                "       |:_/ |\n" +
-                "      //   \\ \\\n" +
-                "     (|     | )\n" +
-                "    /'\\_   _/`\\\n" +
-                "    \\___)=(___/\n";
-        dibujo.setText(tux);*/
+        bEnviar = findViewById(R.id.btnEnviar);
+
     }
 }
